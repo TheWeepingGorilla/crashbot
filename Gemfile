c4e21c6136d12499dc26f4a15a79d7a37fbe09c3
Sean Morrison <student@epicodus.com>
2014-09-01 14:40:45 -0700

    Update bcrypt to 3.1.7.

--- Gemfile
+++ Gemfile
@@ -21,9 +21,14 @@ gem 'turbolinks'
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
 gem 'jbuilder', '~> 2.0'
 
+# Use ActiveModel has_secure_password
+gem 'bcrypt', '~> 3.1.7'
+
 gem 'bootstrap-sass'
 
 gem 'autoprefixer-rails'
+
+
 
 group :development do
   gem 'better_errors'
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140901195642) do
+ActiveRecord::Schema.define(version: 20140901211236) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,14 @@ ActiveRecord::Schema.define(version: 20140901195642) do
     t.integer  "vote"
     t.integer  "rating"
     t.datetime "date"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "name"
+    t.string   "email"
+    t.string   "password_digest"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
